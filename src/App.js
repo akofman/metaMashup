8d76e50b0f85f51bb681376ec1887717662fbfcb
Alexis Kofman <alexis.kofman@gmail.com>
2016-10-23 13:29:52 +0200

    Add assets

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Sidebar from 'react-sidebar';
 import MaterialTitlePanel from './material_title_panel';
 import SidebarContent from './sidebar_content';
+import MashupIntro from './mashup-intro.mp4';
 
 const styles = {
   contentHeaderMenuLink: {
@@ -94,20 +95,9 @@ class App extends Component{
       <Sidebar {...sidebarProps}>
         <MaterialTitlePanel title={contentHeader}>
           <div style={styles.content}>
-            <p>React Sidebar is a sidebar component for React. It offers the following features:</p>
-            <ul>
-              <li>Have the sidebar slide over main content</li>
-              <li>Dock the sidebar next to the content</li>
-              <li>Touch enabled: swipe to open and close the sidebar</li>
-              <li>Easy to combine with media queries for auto-docking (<a href="responsive_example.html">see example</a>)</li>
-              <li>Sidebar and content passed in as PORCs (Plain Old React Components)</li>
-              <li><a href="https://github.com/balloob/react-sidebar">Source on GitHub</a> (MIT license)</li>
-              <li>Only dependency is React</li>
-            </ul>
-            <p><a href="https://github.com/balloob/react-sidebar#installation">Instructions how to get started.</a></p>
-            <p><b>Current rendered sidebar properties:</b></p>
-            {['open', 'docked', 'transitions', 'touch', 'shadow', 'pullRight'].map(this.renderPropCheckbox)}
-            {['touchHandleWidth', 'dragToggleDistance'].map(this.renderPropNumber)}
+          <video width="100%" autoPlay>
+            <source src={ MashupIntro } type="video/mp4"/>
+          </video>
           </div>
         </MaterialTitlePanel>
       </Sidebar>
